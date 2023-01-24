df64c90ba1c29aed8b2a6e3cac1dc8c549df350b
cagespence <cagespence@gmail.com>
2023-01-24 16:54:44 +0100

    add demo

---
+++ src/components/TokensDemonstration/TokensDemonstration.jsx
@@ -0,0 +1,78 @@
+import './styles.css'
+
+const Component = () => (
+  <div className='wrapper'>
+    <div className='sizes'>
+      <div className='group'>
+        <span className='label'>4</span>
+        <div className="size size-4"></div>
+      </div>
+      <div className='group'>
+        <span className='label'>8</span>
+        <div className="size size-8"></div>
+      </div>
+      <div className='group'>
+        <span className='label'>12</span>
+        <div className="size size-12"></div>
+      </div>
+      <div className='group'>
+        <span className='label'>16</span>
+        <div className="size size-16"></div>
+      </div>
+      <div className='group'>
+        <span className='label'>20</span>
+        <div className="size size-20"></div>
+      </div>
+      <div className='group'>
+        <span className='label'>24</span>
+        <div className="size size-24"></div>
+      </div>
+      <div className='group'>
+        <span className='label'>28</span>
+        <div className="size size-28"></div>
+      </div>
+      <div className='group'>
+        <span className='label'>32</span>
+        <div className="size size-32"></div>
+      </div>
+      <div className='group'>
+        <span className='label'>36</span>
+        <div className="size size-36"></div>
+      </div>
+      <div className='group'>
+        <span className='label'>40</span>
+        <div className="size size-40"></div>
+      </div>
+      <div className='group'>
+        <span className='label'>44</span>
+        <div className="size size-44"></div>
+      </div>
+      <div className='group'>
+        <span className='label'>48</span>
+        <div className="size size-48"></div>
+      </div>
+      <div className='group'>
+        <span className='label'>64</span>
+        <div className="size size-64"></div>
+      </div>
+      <div className='group'>
+        <span className='label'>80</span>
+        <div className="size size-80"></div>
+      </div>
+      <div className='group'>
+        <span className='label'>96</span>
+        <div className="size size-96"></div>
+      </div>
+      <div className='group'>
+        <span className='label'>112</span>
+        <div className="size size-112"></div>
+      </div>
+      <div className='group'>
+        <span className='label'>128</span>
+        <div className="size size-128"></div>
+        </div>
+    </div>
+  </div>
+)
+
+export default Component
